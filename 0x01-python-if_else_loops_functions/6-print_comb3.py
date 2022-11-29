64b8b59c584c728cfa79fb6f849281f0371bee2e
Abigail Ejeh <ejehabigail58@gmail.com>
2022-11-29 10:47:05 -0800

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for 1 in range(100):
+    if int(i / 10) != i % 10 and int (i / 10) < i % 10:
+        print("{}{}".format(int(i / 10), end="")
+                if (i != 89):
+                print(", ", end="")
+print("")
