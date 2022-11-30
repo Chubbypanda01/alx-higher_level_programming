7a3d98f3152093f60b52dea1cc23e79862fefc02
Abigail Ejeh <ejehabigail58@gmail.com>
2022-11-29 19:26:27 -0800

    Task 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# Author -Abigail Ejeh
+
 def magic_caculation(a, b, c):
-    if (a < b):
-        return (c\0
-                elif ( c > b:
-                    return (a + b)
-                    else:
-                    return (a * b - c)
+    if a < b:
+        return (c)
+    if c > b:
+        return ( a + b)
+    return (a*b - c)
