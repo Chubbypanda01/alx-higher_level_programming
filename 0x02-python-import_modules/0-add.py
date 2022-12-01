b70a3a9d513902e0c3d8ddb22362d90f3dd19c5a
Abigail Ejeh <ejehabigail58@gmail.com>
2022-12-01 09:54:30 -0800

    Task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name == "__main__":
+    """print the sum of 1 ans 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
